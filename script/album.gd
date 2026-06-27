aee1dee80f9b055a3c65c618c76a1f53f99615f8
Wafiirbn <robbarneyy1@gmail.com>
2026-06-28 01:00:45 +0700

    bismillah gkv a

--- script/album.gd
+++ script/album.gd
@@ -1,86 +1,53 @@
 extends Control
 
-@onready var ikan_list = $ScrollContainer/GridContainer
 @onready var jumlah_label = $JumlahLabel
 @onready var tutup_button = $TutupButton
-
-const WARNA_RARITY = {
-	"Umum":   Color("#2e7d32"),
-	"Langka": Color("#1565c0"),
-	"Epik":   Color("#6a1b9a"),
-	"Legenda": Color("#e65100")
-}
-
-const WARNA_BADGE = {
-	"Umum":   Color("#4caf50"),
-	"Langka": Color("#2196f3"),
-	"Epik":   Color("#9c27b0"),
-	"Legenda": Color("#ff9800")
-}
+@onready var card_ikan = $CardIkan
+@onready var button_sapu2 = $GridContainer/ButtonSapu2
+@onready var button_mujair = $GridContainer/ButtonMujair
+@onready var button_sapu2_albino = $GridContainer/ButtonSapu2Albino
+@onready var button_mas = $GridContainer/ButtonMas
 
 func _ready():
 	tutup_button.pressed.connect(_tutup_album)
-	_tampilkan_koleksi()
+	button_sapu2.pressed.connect(_on_button_sapu2_pressed)
+	button_mujair.pressed.connect(_on_button_mujair_pressed)
+	button_sapu2_albino.pressed.connect(_on_button_sapu2_albino_pressed)
+	button_mas.pressed.connect(_on_button_mas_pressed)
+	
+	var group = ButtonGroup.new()
+	button_sapu2.button_group = group
+	button_mujair.button_group = group
+	button_sapu2_albino.button_group = group
+	button_mas.button_group = group
+	
+	# Disable tombol yang belum ditangkap
+	button_sapu2.disabled = not _sudah_ditangkap("Sapu-sapu Biasa")
+	button_mujair.disabled = not _sudah_ditangkap("Mujair")
+	button_sapu2_albino.disabled = not _sudah_ditangkap("Sapu-sapu Albino")
+	button_mas.disabled = not _sudah_ditangkap("Mas")
+	
+	# Default card = locked kalau belum ada ikan sama sekali
+	if Global.album_koleksi.is_empty():
+		card_ikan.texture = load("res://asset/album_locked.png")
 
-func _tampilkan_koleksi():
-	for child in ikan_list.get_children():
-		child.queue_free()
+func _sudah_ditangkap(nama: String) -> bool:
+	for item in Global.album_koleksi:
+		if item["nama"] == nama:
+			return true
+	return false
 
-	var koleksi = Global.album_koleksi
-	jumlah_label.text = "Koleksi: %d ikan" % koleksi.size()
+func _on_button_sapu2_pressed() -> void:
+	card_ikan.texture = load("res://asset/album_sapu2.png")
 
-	for data in koleksi:
-		ikan_list.add_child(_buat_kartu(data))
+func _on_button_mujair_pressed() -> void:
+	card_ikan.texture = load("res://asset/album_mujair.png")
 
-func _buat_kartu(data: Dictionary) -> Control:
-	var kelangkaan = data.get("kelangkaan", "Umum")
+func _on_button_sapu2_albino_pressed() -> void:
+	card_ikan.texture = load("res://asset/album_sapu2albino.png")
 
-	# Panel utama kartu
-	var panel = PanelContainer.new()
-	panel.custom_minimum_size = Vector2(130, 160)
-	var style = StyleBoxFlat.new()
-	style.bg_color = Color("#1a2a3a")
-	style.border_color = WARNA_RARITY.get(kelangkaan, Color("#2e7d32"))
-	style.set_border_width_all(3)
-	style.set_corner_radius_all(10)
-	panel.add_theme_stylebox_override("panel", style)
-
-	var vbox = VBoxContainer.new()
-	vbox.add_theme_constant_override("separation", 4)
-	panel.add_child(vbox)
-
-	# Nama ikan (atas)
-	var nama_label = Label.new()
-	nama_label.text = data["nama"].to_upper()
-	nama_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-	nama_label.add_theme_font_size_override("font_size", 11)
-	nama_label.add_theme_color_override("font_color", Color.WHITE)
-	vbox.add_child(nama_label)
-
-	# Gambar ikan
-	var img = TextureRect.new()
-	img.texture = data["texture"]
-	img.custom_minimum_size = Vector2(110, 90)
-	img.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
-	img.expand_mode = TextureRect.EXPAND_FIT_WIDTH_PROPORTIONAL
-	vbox.add_child(img)
-
-	# Badge kelangkaan (bawah)
-	var badge_panel = PanelContainer.new()
-	var badge_style = StyleBoxFlat.new()
-	badge_style.bg_color = WARNA_BADGE.get(kelangkaan, Color("#4caf50"))
-	badge_style.set_corner_radius_all(6)
-	badge_panel.add_theme_stylebox_override("panel", badge_style)
-
-	var badge_label = Label.new()
-	badge_label.text = kelangkaan
-	badge_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-	badge_label.add_theme_font_size_override("font_size", 10)
-	badge_label.add_theme_color_override("font_color", Color.WHITE)
-	badge_panel.add_child(badge_label)
-	vbox.add_child(badge_panel)
-
-	return panel
+func _on_button_mas_pressed() -> void:
+	card_ikan.texture = load("res://asset/album_mas.png")
 
 func _tutup_album():
 	get_tree().paused = false
