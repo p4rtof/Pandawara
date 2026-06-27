4b1a4e8b125087293abbcf66da92a6fc372250e6
Fadhla <fadhla153@gmail.com>
2026-06-27 18:42:39 +0700

    fix: score gameover

--- script/gameover.gd
+++ script/gameover.gd
@@ -4,17 +4,17 @@ extends Control
 @onready var restart_button = $RestartButton
 
 func _ready():
-	# Tampilkan skor akhir
 	score_label.text = "Skor Akhir: " + str(Global.poin)
-	
-	# Sambung tombol restart
 	restart_button.pressed.connect(_on_restart_pressed)
 
 func _on_restart_pressed():
-	# Reset semua data
+	# Reset progress di bioma yang sedang dimainkan
 	Global.poin = 0
 	Global.nyawa = 5
 	Global.album_koleksi.clear()
-	
-	# Balik ke game
+	Global.sapu_sapu_ditangkap = 0          # ← INI YANG KURANG
+	Global.sedang_game_over = false         # ← biar ikan.gd bisa proses ulang
+	Global.target_sapu_sapu = Global.target_sapu_sapu_per_bioma[Global.bioma_dipilih]  # ← jaga2 kalau target per bioma beda
+
+	# Balik ke bioma yang sama (Global.bioma_dipilih TIDAK direset, jadi tetap di bioma yang sama)
 	get_tree().change_scene_to_file("res://scene/game_sungai.tscn")
