c9250c322fc858faf81f816f5f50fb44c70f1cbb
Fadhla <fadhla153@gmail.com>
2026-06-28 16:59:49 +0700

    fix:pilih bioma

--- script/pilih_bioma.gd
+++ script/pilih_bioma.gd
@@ -1,18 +1,17 @@
 extends Control
 
 var album_scene = preload("res://scene/album.tscn")
-
-@onready var koleksi_button = $KoleksiIkan   # ← BARU
+@onready var koleksi_button = $KoleksiIkan
 
 func _ready():
 	$KembaliButton.pressed.connect(_kembali)
-	koleksi_button.pressed.connect(_buka_album)   # ← BARU
+	koleksi_button.pressed.connect(_buka_album)
 
-	_setup_kartu(0, $HBoxContainer/KartuHulu/GambarCard/ButtonLanjut, null)
-	_setup_kartu(1, $HBoxContainer/KartuPertengahan/GambarCard/ButtonLanjut, $HBoxContainer/KartuPertengahan/GambarCard/ColorRect)
-	_setup_kartu(2, $HBoxContainer/KartuPerkotaan/GambarCard/ButtonLanjut, $HBoxContainer/KartuPerkotaan/GambarCard/ColorRect)
+	_setup_kartu(0, $HBoxContainer/CardHulu/ButtonLanjut, null)
+	_setup_kartu(1, $HBoxContainer/CardTengah/ButtonLanjut, $HBoxContainer/CardTengah/ColorRect)
+	_setup_kartu(2, $HBoxContainer/CardKota/ButtonLanjut, $HBoxContainer/CardKota/ColorRect)
 
-func _buka_album():   # ← BARU
+func _buka_album():
 	get_tree().paused = true
 	if get_tree().get_first_node_in_group("album"):
 		return
@@ -22,12 +21,9 @@ func _buka_album():   # ← BARU
 
 func _setup_kartu(index: int, tombol: BaseButton, lock_overlay: ColorRect):
 	var terkunci = index > Global.bioma_terbuka
-
 	if lock_overlay:
 		lock_overlay.visible = terkunci
-
 	tombol.disabled = terkunci
-
 	if not terkunci:
 		tombol.pressed.connect(_pilih_bioma.bind(index))
 
@@ -37,7 +33,6 @@ func _pilih_bioma(index: int):
 	Global.nyawa = 5
 	Global.sedang_game_over = false
 	Global.target_sapu_sapu = Global.target_sapu_sapu_per_bioma[index]
-
 	if Global.siapkan_story_intro(index):
 		get_tree().change_scene_to_file("res://scene/story_transisi.tscn")
 	else:
